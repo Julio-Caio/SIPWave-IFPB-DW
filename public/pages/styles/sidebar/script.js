59c6c53b46eb464a6aa6604c4016a034f3f7f807
Julio-Caio <julio.caio@academico.ifpb.edu.br>
2024-11-16 18:41:21 -0300

    Adição Dashboard | Ajuste no Footer e links

--- public/pages/styles/sidebar/script.js
+++ public/pages/styles/sidebar/script.js
@@ -1,6 +1,5 @@
 function toggleSidebar()
-{
-    const menu = document.querySelector("#toggleSidebarButton i")
+{    
     const sidebar = document.getElementById("collapseSidebar")
 
     sidebar.classList.toggle("active")
